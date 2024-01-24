f953867adced5d5cc9ed0929a95ddff86e089fcb
Rijan.Maharjan <rijan.maharjan@amniltech.com>
2024-01-24 22:57:18 +0545

    all 2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
-
+app.get("/ping", (req, res) => {
+  res.json("pong")
+  return;
+})
 app.listen(3333, () =>
 {
   console.info(`app listening at http://localhost:3333`)
